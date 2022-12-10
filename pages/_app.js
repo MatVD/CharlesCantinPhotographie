ffac4616c5655190fad5a16b8d4dfb72ea1647af
MatVD <vandammemathieu@hotmail.fr>
2022-12-10 08:03:12 +0100

    add css on layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from "../components/Layout";
+import Layout from "../components/Layout/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
-      <h1>Home page</h1>
+      <h1 className={styles.h1}>Home page</h1>
     </>
   )
 }
