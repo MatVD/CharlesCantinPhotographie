7dcc93a491345bf6962a3ceaea43121de7c0cdbe
MatVD <vandammemathieu@hotmail.fr>
2022-12-11 23:15:15 +0100

    add isActive on links

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -3,55 +3,66 @@ import React from "react";
 import styles from "./Navbar.module.css";
 import { useState } from "react";
 import Image from "next/image";
-import Logo from "../../public/logo-charles-cantin.png"
+import Logo from "../../public/logo-charles-cantin.png";
 import Insta from "../../public/Instagram.png";
-import Facebook from "../../public/Facebook.png"
+import Facebook from "../../public/Facebook.png";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
   const [navOpen, setNavOpen] = useState(false);
 
+  const router = useRouter();
+
+  const links = [
+    {
+      href: "/",
+      name: "Accueil",
+    },
+    {
+      href: "/galerie",
+      name: "Galerie",
+    },
+    {
+      href: "/tarifs-presta",
+      name: "Tarifs et prestations",
+    },
+    {
+      href: "/contact",
+      name: "Contact",
+    },
+  ];
+
   return (
     <header className={styles.header}>
+      {/* La nav à un style par défaut, au clique styles.show est rajouté */}
       <nav className={`${styles.nav} ${navOpen ? styles.show : null}`}>
         <div className={styles.imageContainer}>
-          <Image
-            src={Logo}
-            width={76}
-            height={76}
-            priority
-            alt="Logo"
-          />
+          <Image src={Logo} width={76} height={76} priority alt="Logo" />
           <div className={styles.socialContainer}>
-            <Image
-            src={Facebook}
-            width={32}
-            height={32}
-            alt="Facebook" 
-            />
-            <Image
-            src={Insta}
-            width={32}
-            height={32}
-            alt="Instagram" 
-            />
+            <Image src={Facebook} width={32} height={32} alt="Facebook" />
+            <Image src={Insta} width={32} height={32} alt="Instagram" />
           </div>
         </div>
-        {/* La nav à un style par défaut, au clique styles.show est rajouté */}
-          <ul className={styles.ul}>
-            <li className={styles.li}>
-              <Link href="/">Accueil</Link>
+        <ul className={styles.ul}>
+          {links.map((link, i) => (
+            <li className={styles.li} key={i}>
+              <Link
+                href={link.href}
+                className={
+                  router.pathname == `/`
+                    ? `${styles.linkUnderline}`
+                    : ""
+                }
+              >
+                {link.name}
+              </Link>
             </li>
-            <li className={styles.li}>
-              <Link href="/galerie">Galerie</Link>
-            </li>
-            <li className={styles.li}>
-              <Link href="/tarifs-presta">Tarifs et prestations</Link>
-            </li>
-            <li className={styles.li}>
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-        <button className={styles.btnBurger} onClick={() => setNavOpen(!navOpen)}>
+          ))}
+        </ul>
+        <button
+          className={styles.btnBurger}
+          onClick={() => setNavOpen(!navOpen)}
+        >
           <span className={styles.burgerBar}></span>
         </button>
       </nav>
