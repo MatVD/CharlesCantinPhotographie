7a1c842461faab8eb9af5e2be7c8c4e62fd167a1
MatVD <vandammemathieu@hotmail.fr>
2022-12-17 23:01:08 +0100

    add emailjs on form

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,46 +1,8 @@
 import styles from "../styles/contact.module.css";
 import Head from "next/head";
+import handleSubmit from "../lib/HandleForm";
 
 const contact = () => {
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    // Toast info
-    const formInfo = document.getElementById("formInfos");
-    formInfo.innerText = "";
-
-    // Récupération data du formulaire
-    const data = {
-      name: event.target.name.value,
-      email: event.target.email.value,
-      message: event.target.message.value,
-    };
-
-    // Envoie des datas
-    const JSONdata = JSON.stringify(data);
-
-    const response = await fetch("/api/form", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSONdata,
-    });
-
-    // Reset du form et ajout d'un toast "Message envoyé"
-    if (response.status === 200) {
-      event.target.name.value = "";
-      event.target.email.value = "";
-      event.target.message.value = "";
-      formInfo.style.display = "flex";
-      formInfo.append("Message envoyé");
-      setTimeout(() => {
-        formInfo.style.display = "none";
-      }, 7000);
-    }
-
-    const result = await response.json();
-  };
 
   return (
     <>
@@ -58,7 +20,13 @@ const contact = () => {
         <div className={styles.wrapperForm}>
           <form onSubmit={handleSubmit} className={styles.form}>
             <label htmlFor="name">Nom et Prénom</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              minLength={1}
+              maxLength={50}
+            />
 
             <label htmlFor="email">Email</label>
             <input
@@ -66,10 +34,12 @@ const contact = () => {
               id="email"
               pattern="[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})"
               required
+              minLength={2}
+              maxLength={60}
             />
 
             <label htmlFor="message">Message</label>
-            <textarea type="text" id="message" required rows={9} />
+            <textarea type="text" id="message" required rows={9} maxLength={200}/>
 
             <button type="submit">Envoyer</button>
           </form>
